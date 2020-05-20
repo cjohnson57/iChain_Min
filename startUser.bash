e0acceec23eeb7746ed18c3b8cd2bcd6114d9575
cjohnson57 <c.johnson.57@outlook.com>
2020-05-19 20:43:33 -0500

    Initial commit

---
+++ startUser.bash
@@ -0,0 +1,3 @@
+cd ML/localUser
+gnome-terminal --title="BCAI User Console" --tab -x ./userCLI 
+gnome-terminal --title="BCAI User File Hosting/Transfer" --tab -x sudo ./execute
---
+++ startWorker.bash
@@ -0,0 +1,3 @@
+cd ML/localWorker
+gnome-terminal --title="BCAI Worker Console" --tab -x ./workerCLI 
+gnome-terminal --title="BCAI Worker File Hosting/Transfer" --tab -x sudo ./execute
