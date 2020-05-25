7ce8c8ff448e690160714c8babd43550ef43db9d
Caleb Johnson <c.johnson.57@outlook.com>
2020-05-25 11:25:51 -0500

    Changed BCAI to iChain in startUser

--- startUser.bash
+++ startUser.bash
@@ -1,3 +1,3 @@
 cd ML/localUser
-gnome-terminal --title="BCAI User Console" --tab -x ./userCLI 
-gnome-terminal --title="BCAI User File Hosting/Transfer" --tab -x sudo ./execute
+gnome-terminal --title="iChain User Console" --tab -x ./userCLI 
+gnome-terminal --title="iChain User File Hosting/Transfer" --tab -x sudo ./execute
