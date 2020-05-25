45351b6f2b2de380941c533d2d38e59555a12fc8
Caleb Johnson <c.johnson.57@outlook.com>
2020-05-25 11:26:14 -0500

    Changed BCAI to iChain in startWorker

--- startWorker.bash
+++ startWorker.bash
@@ -1,3 +1,3 @@
 cd ML/localWorker
-gnome-terminal --title="BCAI Worker Console" --tab -x ./workerCLI 
-gnome-terminal --title="BCAI Worker File Hosting/Transfer" --tab -x sudo ./execute
+gnome-terminal --title="iChain Worker Console" --tab -x ./workerCLI 
+gnome-terminal --title="iChain Worker File Hosting/Transfer" --tab -x sudo ./execute
